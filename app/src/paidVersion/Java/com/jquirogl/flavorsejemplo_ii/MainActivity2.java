ae2f3b403f1f2041228c5b066bd07523bfd41c54
Jamer <jquirogl@everis.com>
2017-11-08 15:19:49 -0500

    Upload files by Jamer Quiroga

---
+++ app/src/paidVersion/Java/com/jquirogl/flavorsejemplo_ii/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.jquirogl.flavorsejemplo_ii;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by jquirogl on 08/11/2017.
+ */
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @BindView(R.id.myTextView)
+    TextView myTextView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        myTextView.setText("App de paga");
+    }
+}
